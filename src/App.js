a3c5dd7b3142a6ff2a5b7100a247692f6c984334
Kristina Zhuravleva <kris07081992@gmail.com>
2026-02-06 11:59:34 +0100

    inicio de login form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
+import Login from "/pages/Login/Login.js";
 
 function App() {
   return (
     <div className="container bg-primary"><i class="bi bi-0-square-fill"></i>Test</div>
+      <>
+      <Login />
+    </>
   );
 }
 
