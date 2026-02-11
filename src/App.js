eaedc8621bd1392bcd87664bcfcc5e11caf68f98
Lina <exclusiva@tuta.io>
2026-02-11 13:53:49 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.scss';
+import Footer from './components/Footer/Footer'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
-    <div className="container bg-primary"><i class="bi bi-0-square-fill"></i>Test</div>
+    <div className="container bg-primary"><i class="bi bi-0-square-fill"></i>Test
+ 
+       <Footer />  
+    </div>
   );
 }
 
