3ac93638d1b564a52eaef114ab2e6d948999c7af
Elshin Vadim <elshinvadim@gmail.com>
2026-02-24 23:29:27 +0100

    Getting started

--- src/pages/Router/AppRouter.js
+++ src/pages/Router/AppRouter.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../Home/Home";
 import Settings from "../Settings/Settings";
 import MisRutas from "../MisRutas/MisRutas";
+import Mapa from "../Mapa/Mapa";
 
 export default function AppRouter(){
     return (
         <>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/map" element={<Mapa />} />
                 <Route path="/my-routes" element={<MisRutas />} />
                 <Route path="/settings" element={<Settings />} />
             </Routes>
