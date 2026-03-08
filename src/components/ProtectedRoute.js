7c9e30d38b690d59e188f9611202556cea969084
KristinaZhur <kris07081992@gmail.com>
2026-03-08 17:01:35 +0100

    add cambios

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 export default function ProtectedRoute({ children }) {
     const user = JSON.parse(localStorage.getItem("user"));
-   if (!user || !user.admin) {
+   if (!user || !user.administrador) {
     return <Navigate to="/" replace />; 
   }
 
