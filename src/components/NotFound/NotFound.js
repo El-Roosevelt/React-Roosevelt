8a1fc43624f700af6d6be6478f1b3e9b2b043ff4
Lina <exclusiva@tuta.io>
2026-03-09 11:39:25 +0100

    not found cambios en router + e agregado estilos

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,26 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NotFound.scss";  
 
 export default function NotFound() {
   return (
-    // vh-100 делает блок высотой во весь экран, а d-flex центрирует содержимое
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100 text-center bg-white">
+     <div className="not-found-section d-flex flex-column justify-content-center align-items-center vh-100 text-center bg-white px-3">
       
-      {/* Твоя картинка с роботом */}
       <img
-        src="/assets/404-robot.png"
+        src="/assets/404-robot.png" 
         alt="Error 404 - Page not found"
-        className="img-fluid mb-4"
-        style={{ maxWidth: "500px" }} // Ограничиваем размер, чтобы не была слишком огромной
+        className="robot-img img-fluid mb-4"
       />
 
-      {/* Небольшой текст и кнопка возврата */}
       <h2 className="text-primary fw-bold mb-3">¡Ups! Te has perdido...</h2>
+      
       <p className="text-secondary mb-4">
         La página que buscas no existe o ha sido movida.
       </p>
       
-      {/* Кнопка ведет на главную страницу */}
       <Link to="/" className="btn btn-primary px-4 py-2 rounded-pill fw-bold">
         Volver al Inicio
       </Link>
--- src/pages/Router/AppRouter.js
+++ src/pages/Router/AppRouter.js
@@ -19,6 +19,7 @@ export default function AppRouter() {
                 <Route path="/fav-routes" element={<RutasFavoritas />} />
                 <Route path="/settings" element={<Settings />} />
                 <Route path="/map" element={<Mapa />} /> 
+                <Route path="*" element={<NotFound />} />
             </Routes>
 
         </>
