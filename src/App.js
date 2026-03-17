75b4a3f6424d2a8e686536687a9f3e888b549fcd
Elshin Vadim <elshinvadim@gmail.com>
2026-03-17 10:40:32 +0100

    Removed Login from NavBar lg & Added Logout function to Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.scss";
-import './../node_modules/bootstrap/dist/js/bootstrap.min.js'
-import './../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
-import './../node_modules/bootstrap/dist/js/bootstrap.esm.js'
+import "./../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "./../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import "./../node_modules/bootstrap/dist/js/bootstrap.esm.js";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
@@ -12,17 +12,16 @@ import { useState } from "react";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
-  
   return (
     <>
-    <AuthProvider>
-      <BrowserRouter>
-      <ScrollToTop />
-        <Header />
-        <NavBar />
-        <AppRouter />
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <ScrollToTop />
+          <Header />
+          <NavBar />
+          <AppRouter />
+          <Footer />
+        </BrowserRouter>
       </AuthProvider>
     </>
   );
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,86 +1,3 @@
-// import "./NavBar.scss";
-// import { NavLink } from "react-router-dom";
-
-
-
-// export default function NavBar() {
-//   const link = ({ isActive }) =>
-//     "nav-link text-light fs-5" + (isActive ? " active fw-bold" : "");
-
-
-//   return (
-
-//     <>
-//       <div className="d-none d-md-flex w-100 justify-content-center ">
-//         <ul className="container-fluid nav nav-pills nav-fill justify-content-between p-1 bg-secondary">
-
-//           <li className="nav-item col-2"><NavLink to="/" className={link}>
-//             <i className="bi bi-house-door pe-1"></i>
-//             <span className="d-none d-lg-inline">Inicio</span></NavLink></li>
-
-//           <li className="nav-item col-2"><NavLink to="/map" className={link}>
-//             <i className="bi bi-globe-americas pe-1"></i>
-//             <span className="d-none d-lg-inline">Mapa</span></NavLink></li>
-
-//           <li className="nav-item col-2"><NavLink to="/my-routes" className={link}>
-//             <i className="bi bi-geo-alt pe-1"></i>
-//             <span className="d-none d-lg-inline">Mis Rutas</span></NavLink></li>
-
-//           <li className="nav-item col-2"><NavLink to="/fav-routes" className={link}>
-//             <i className="bi bi-heart pe-1"></i>
-//             <span className="d-none d-lg-inline">Rutas Favoritas</span></NavLink></li>
-
-//           <li className="nav-item col-2"><NavLink to="/settings" className={link}>
-//             <i className="bi bi-gear pe-1"></i>
-//             <span className="d-none d-lg-inline">Configuración</span></NavLink></li>
-
-//         </ul>
-//       </div>
-//       <div className="d-flex w-100 d-md-none">
-//         <nav className="w-100 navbar navbar-expand-lg bg-body-tertiary">
-//           <div className="container-fluid">
-//             <a className="navbar-brand" href="#">Navbar</a>
-//             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//               <span className="navbar-toggler-icon"></span>
-//             </button>
-//             <div className="collapse navbar-collapse" id="navbarNav">
-//               <ul className="navbar-nav">
-//                 <li className="nav-item">
-//                   <NavLink to="/" className="nav-link">
-//                     <i className="bi bi-house-door pe-1"></i>
-//                     <span className="d-lg-inline">Inicio</span></NavLink>
-//                 </li>
-//                 <li className="nav-item">
-//                   <NavLink to="/map" className="nav-link">
-//                     <i className="bi bi-globe-americas pe-1"></i>
-//                     <span className="d-lg-inline">Mapa</span></NavLink>
-//                 </li>
-//                 <li className="nav-item">
-//                   <NavLink to="/my-routes" className="nav-link">
-//                     <i className="bi bi-geo-alt pe-1"></i>
-//                     <span className="d-lg-inline">Mis Rutas</span></NavLink>
-//                 </li>
-//                 <li className="nav-item">
-//                   <NavLink to="/fav-routes" className="nav-link">
-//                     <i className="bi bi-heart pe-1"></i>
-//                     <span className="d-lg-inline">Rutas Favoritas</span></NavLink>
-//                 </li>
-//                 <li className="nav-item">
-//                   <NavLink to="/settings" className="nav-link">
-//                     <i className="bi bi-gear pe-1"></i>
-//                     <span className="d-lg-inline">Configuración</span></NavLink>
-//                 </li>
-//               </ul>
-//             </div>
-//           </div>
-//         </nav>
-//       </div>
-//     </>
-
-//   );
-// }
-
-
 import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import "./NavBar.scss";
@@ -171,19 +88,6 @@ export default function NavBar() {
               <span className="d-none d-lg-inline">Configuración</span>
             </NavLink>
           </li>
-
-          <li className="nav-item col">
-            {user ? (
-              <span role="button" className=" nav-link text-light fs-5" onClick={handleLogout}> 
-              {/* nav-links text-light border-0 bg-transparent  */}
-                <i className="bi bi-box-arrow-right me-1"></i>Cerrar sesión
-              </span>
-            ) : (
-              <NavLink to="/login" className={link("/login")}>
-                <i className="bi bi-person-circle pe-1"></i>Login
-              </NavLink>
-            )}
-          </li> 
         </ul>
       </div>
 
